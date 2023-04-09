a0d77eaf3367c076e236882e1747e75a130c59fd
Sergey Ternovtsov <tob1az@ukr.net>
2023-04-09 22:53:57 +0300

    Move to clap

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
 #[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
 use std::fs;
-
 use std::io::Result;
+use clap::{Parser, Subcommand};
 
-fn init() -> Result<()> {
-    fs::create_dir(".git")?;
-    fs::create_dir(".git/objects")?;
-    fs::create_dir(".git/refs")?;
-    fs::write(".git/HEAD", "ref: refs/heads/master\n")
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
 }
 
-fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[derive(Subcommand, Debug)]
+enum Command {
+    Init,
+}
 
-    // Uncomment this block to pass the first stage
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        init()?;
-        println!("Initialized git directory");
-    } else {
-        println!("unknown command: {}", args[1]);
+impl Command  {
+    fn run(&self) -> Result<()> {
+        match self {
+            Self::Init => {
+                fs::create_dir(".git")?;
+                fs::create_dir(".git/objects")?;
+                fs::create_dir(".git/refs")?;
+                fs::write(".git/HEAD", "ref: refs/heads/master\n")
+            }
+        }
     }
+}
+
+
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    args.command.run()?;
     Ok(())
 }
