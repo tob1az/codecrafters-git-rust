a784ef285237dbc6f2377988ccd8e931f3a17f85
Sergey Ternovtsov <tob1az@ukr.net>
2023-04-09 22:28:21 +0300

    Handle errors

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,26 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
-fn init() {
-    fs::create_dir(".git").unwrap();
-    fs::create_dir(".git/objects").unwrap();
-    fs::create_dir(".git/refs").unwrap();
-    fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+use std::io::Result;
+
+fn init() -> Result<()> {
+    fs::create_dir(".git")?;
+    fs::create_dir(".git/objects")?;
+    fs::create_dir(".git/refs")?;
+    fs::write(".git/HEAD", "ref: refs/heads/master\n")
 }
 
-fn main() {
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
-        init();
-        println!("Initialized git directory")
+        init()?;
+        println!("Initialized git directory");
     } else {
-        println!("unknown command: {}", args[1])
+        println!("unknown command: {}", args[1]);
     }
+    Ok(())
 }
