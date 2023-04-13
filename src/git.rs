ab58e462afbd81270b8dd7e47ea458c3af9f71e6
Sergey Ternovtsov <tob1az@ukr.net>
2023-04-13 15:43:17 +0300

    Support creating blobs

--- src/git.rs
+++ src/git.rs
@@ -1,7 +1,8 @@
-use flate2::read::ZlibDecoder;
-use std::io::{prelude::*, stdout, Error, ErrorKind, Result};
-use std::path::PathBuf;
+use flate2::{read::ZlibDecoder, write::ZlibEncoder, Compression};
+use sha1::{Digest, Sha1};
 use std::fs;
+use std::io::{prelude::*, stdout, Error, ErrorKind, Result};
+use std::path::{Path, PathBuf};
 
 pub struct Object {
     content: Vec<u8>,
@@ -13,8 +14,7 @@ impl Object {
         if hash.len() != HASH_SIZE {
             return Err(Error::from(ErrorKind::InvalidInput));
         }
-        let (subdir, filename) = hash
-            .split_at(2);
+        let (subdir, filename) = hash.split_at(2);
         let mut filepath = PathBuf::new();
         filepath.push(".git");
         filepath.push("objects");
@@ -37,3 +37,32 @@ impl Object {
     }
 }
 
+pub type Hash = String;
+
+pub fn blobify(filepath: &Path) -> Result<Hash> {
+    let content_size = filepath.metadata()?.len() as usize;
+    const HEADER_APPROX_SIZE: usize = 20;
+    let mut blob = Vec::with_capacity(HEADER_APPROX_SIZE + content_size);
+    blob.write(b"blob ")?;
+    blob.write(content_size.to_string().as_bytes())?;
+    blob.write(&[0])?;
+    let header_size = blob.len();
+    blob.resize(header_size + content_size, 0);
+    fs::File::open(filepath)?.read_exact(&mut blob[header_size..])?;
+
+    let mut hasher = Sha1::new();
+    hasher.update(&blob);
+    let hash = hex::encode(hasher.finalize());
+    let (subdir, filename) = hash.split_at(2);
+    let mut filepath = PathBuf::new();
+    filepath.push(".git");
+    filepath.push("objects");
+    filepath.push(subdir);
+    fs::create_dir_all(filepath.clone())?;
+    filepath.push(filename);
+    let file = fs::File::create(filepath)?;
+    let mut encoder = ZlibEncoder::new(file, Compression::best());
+    encoder.write_all(&blob)?;
+    encoder.finish()?;
+    Ok(hash)
+}
